e5f5aa66b79451a0be26f25e3b28c36c283c1da0
NicolasPicardi <signos97@gmail.com>
2021-01-19 13:36:17 -0300

    Trying geoLocation and local notifications

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,127 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
+	<ion-page>
+		<ion-header>
+			<ion-toolbar>
+				<ion-title>Blank</ion-title>
+			</ion-toolbar>
+		</ion-header>
+
+		<ion-content>
+			<div id="container">
+				<p>{{ locationData }}</p>
+				<p>{{ notificationsPending }}</p>
+
+				<ion-button size="small" @click="getLocation()"
+					>get location</ion-button
+				>
+				<ion-button size="small" @click="scheduleNotif()"
+					>schedule notification</ion-button
+				>
+			</div>
+		</ion-content>
+	</ion-page>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+	IonButton,
+} from '@ionic/vue'
+import { ref } from 'vue'
+import { Plugins } from '@capacitor/core'
 
-export default defineComponent({
-  name: 'Home',
-  components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-  }
-});
+export default {
+	name: 'Home',
+	components: {
+		IonContent,
+		IonButton,
+		IonHeader,
+		IonPage,
+		IonTitle,
+		IonToolbar,
+	},
+	setup() {
+		const locationData = ref({})
+		const notificationsPending = ref({})
+		const isOpenRef = ref(false)
+
+		const setOpen = (state) => (isOpenRef.value = state)
+
+		const getLocation = async () => {
+			const { Geolocation } = Plugins
+			const results = await Geolocation.getCurrentPosition()
+			locationData.value = {
+				lat: results.coords.latitude,
+				long: results.coords.longitude,
+			}
+		}
+		const scheduleNotif = async () => {
+			const { LocalNotifications } = Plugins
+			const notifs = await LocalNotifications.schedule({
+				notifications: [
+					{
+						title: 'Title',
+						body: 'Body',
+						id: 1,
+						schedule: { at: new Date(Date.now() + 1000 * 5) },
+						sound: null,
+						attachments: null,
+						actionTypeId: '',
+						extra: null,
+					},
+				],
+			})
+			console.log('scheduled notifications', notifs)
+			const pending = await LocalNotifications.getPending()
+			console.warn(pending)
+			notificationsPending.value = pending
+			console.warn(notificationsPending)
+			setOpen(true)
+		}
+
+		return {
+			locationData,
+			getLocation,
+			scheduleNotif,
+			notificationsPending,
+			isOpenRef,
+			setOpen,
+		}
+	},
+	mounted() {},
+}
 </script>
 
 <style scoped>
 #container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+	text-align: center;
+
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 50%;
+	transform: translateY(-50%);
 }
 
 #container strong {
-  font-size: 20px;
-  line-height: 26px;
+	font-size: 20px;
+	line-height: 26px;
 }
 
 #container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
+	font-size: 16px;
+	line-height: 22px;
+
+	color: #8c8c8c;
+
+	margin: 0;
 }
 
 #container a {
-  text-decoration: none;
+	text-decoration: none;
 }
 </style>
